4351feda1c3c015bdd6e759aa46ad37187adcc83
Jacob Cooper <jcooper036@gmail.com>
2019-11-01 10:53:31 -0600

    v4.1.0 - Semi-stable version of AutoBot is working

--- AutoPrimer/autobot/Worker.py
+++ AutoPrimer/autobot/Worker.py
@@ -2,12 +2,14 @@
 
 import datetime
 import AutoPrimer as ntp
+import os
 
 class Worker(object):
 
     def __init__(self, name, command, options, channel, poster):
         # date time stamp from scheduler
         self.name = name
+        self.status = 'init' # init, running, done, expired
         
         # which command this worker should execute
         self.command = command
@@ -20,54 +22,81 @@ class Worker(object):
 
         # defines possible commands
         self.comms = {
-            'find_primers' : self.find_primers()
+            'find_primers' : self.find_primers,
+            'connection_status' : self.connection_status,
+            'hello' : self.hello
         }
-    
-    def start_messge(self):
+
+        # all commands from comms should go into one of these two categories
+        self.message_commands = ['connection_status', 'hello']
+        self.action_commands = ['find_primers']
+
+
+    def start_message(self):
         # start and stop message commands
-        if self.command in ['find_primers']:
+        if self.command in self.action_commands:
             now = datetime.datetime.now()
             mess = f"Starting {self.command} at " + now.strftime("%Y-%m-%d.%H-%M")
             return mess
+        
         # start only message commands
-        if self.command in ['status', 'hello']:
+        if self.command in self.message_commands:
             return self.comms[self.command]()
 
-    def run(self, common_dict):
-        if self.command in ['find_primers']:
-            common_dict[self.name]['status'] = 'running'
+    def run(self):
+        if self.command in self.action_commands:
             self.comms[self.command]()
-            common_dict[self.name]['status'] = 'done'
 
-    def done_messge(self):
-        if self.command in ['find_primers']:
+    def done_message(self):
+        if self.command in self.action_commands:
             now = datetime.datetime.now()
             mess = f"Finshed {self.command} main at " + now.strftime("%Y-%m-%d.%H-%M")
             common_dict[self.name]['status'] = 'closed'
+            self.status = 'expired'
             return mess
-        else:
-            return False
     
     #######
     ##### Commands
     #######
 
     def find_primers(self):
-        ntp.submit_folder(self.options)
-        return
-
-    def status(self):
-        find_folder = os.path.isdir(self.options)
+        """
+        Initiates AutoPrimer
+        """
+        self.status = 'running'
+        if not self.options:
+            self.options = '/Volumes/i_bio/Crispr_F0_Screens/0-Genes_for_design/Genes_for_autoprimer'
+        find_folder = os.path.isdir('/Volumes/i_bio/Crispr_F0_Screens/0-Genes_for_design/Genes_for_autoprimer')
         if find_folder:
-            response = "Everything looks good, ready to run"
+            ntp.submit_folder(self.options)
+            self.status = 'done'
         else:
-            response = "I can't find the autoprimer gene folder - it is possible that the connection is bad"
-        slack_client.api_call(
-            "chat.postMessage",
-            channel=channel,
-            text=response
-    )    
+            self.status = 'failed'
     
+    def connection_status(self):
+        """
+        Checks the connection status to a given folder
+        """
+        if self.options:
+            folder = self.options
+            find_folder = os.path.isdir(self.options)
+            
+        else:
+            folder = '/Volumes/i_bio/Crispr_F0_Screens/0-Genes_for_design/Genes_for_autoprimer'
+            find_folder = os.path.isdir('/Volumes/i_bio/Crispr_F0_Screens/0-Genes_for_design/Genes_for_autoprimer')
+        
+        if find_folder:
+            response = f"Connection established with {folder}. Everything looks good, ready to run"
+        else:
+            response = f"I can't find the {folder} - it is possible that the connection is bad"
+        
+        self.status = 'expired'
+        return response
+
     def hello(self):
+        """
+        Replies to the user with a friendly hello message.
+        """
         response = f"Hello <@{self.poster}>! Looking forward to designing some primers for you."
+        self.status = 'expired'
         return response
--- AutoPrimer/autobot/autobot.py
+++ AutoPrimer/autobot/autobot.py
@@ -1,7 +1,8 @@
 #! /usr/bin/env python3
 
-#https://www.fullstackpython.com/blog/build-first-slack-bot-python.html
+# https://www.fullstackpython.com/blog/build-first-slack-bot-python.html
 # export SLACK_BOT_TOKEN=xoxb-2984982615-762823711127-RU9n3ojzHVkmqwI0VLVakxMq
+# source autoprimer/bin/activate
 import AutoPrimer as ntp
 import os
 import time
@@ -19,7 +20,7 @@ def parse_bot_commands(slack_events):
         if event["type"] == "message" and not "subtype" in event:
             user_id, message = parse_direct_mention(event["text"])
             if user_id == autobot_id:
-                return message, event["channel"], event["user"]
+                return message.lower(), event["channel"], event["user"]
     return None, None, None
 
 def parse_direct_mention(message_text):
@@ -41,39 +42,95 @@ def parse_command(command, com):
         return False
     return options
 
-def handle_command(command, channel):
+def handle_command(command, channel, poster):
     """
         Executes bot command if the command is known
     """
     # Default response is help text for the user
-    default_response = "Not sure what you mean. Try *{}*.".format(COMMANDS)
+    wkr_commands = ['find_primers', 'connection_status', 'hello'] # list of commands needs to be updated as Worker class is
+    default_response = "Not sure what you mean. Try *{}*.".format(wkr_commands)
+    now = datetime.datetime.now()
 
     # main loop to look for commands    
     found_command = False
-    for com in COMMANDS:
+    for com in wkr_commands:
         if command.startswith(com):
             found_command = True
 
             # parse the command options
             options = parse_command(command, com)
 
+            # debug
+            # print(now, command, options, channel, poster)
+
             # add the job to the schedule
-            SCHEDULE[now] = {
-                'bot':Worker(now, command, options, channel, poster),
-                'status' : 'init'
-            }
+            SCHEDULE[now] = ntp.Worker(now, command, options, channel, poster)
     
     # Default response
     if not found_command:
-        options = True
-        COMMANDS['help'](options)
+        catch_message = f"I didn't recognize that command. Try using one of these commands : {','.join(wkr_commands)}"
+        post(catch_message, channel)
 
-def post(message):
+def post(message, channel):
     slack_client.api_call(
         "chat.postMessage",
         channel=channel,
         text=message
-    )  
+    )
+
+########################
+###### Run
+########################
+
+# instantiate Slack client
+slack_client = SlackClient(os.environ.get('SLACK_BOT_TOKEN'))
+# autobot's user ID in Slack: value is assigned after the bot starts up
+autobot_id = None
+
+# DEFAULT_FOLDER = '/Volumes/i_bio/Crispr_F0_Screens/0-Genes_for_design/Genes_for_autobot_beta/'
+DEFAULT_FOLDER = '/mnt/i_bio/Crispr_F0_Screens/0-Genes_for_design/Genes_for_autobot_beta/'
+
+
+# constants
+RTM_READ_DELAY = 1 # 1 second delay between reading from RTM
+# COMMANDS = {
+#     "make_primers" : make_primers,
+#     "hello": hello,
+#     "help" : help,
+#     "status" : status,
+# }
+MENTION_REGEX = "^<@(|[WU].+?)>(.*)"
+SCHEDULE = {}
+
+if __name__ == "__main__":
+    if slack_client.rtm_connect(with_team_state=False):
+        print("AutoBot connected and running!")
+        # Read bot's user ID by calling Web API method `auth.test`
+        autobot_id = slack_client.api_call("auth.test")["user_id"]
+        while True:
+            command, channel, poster = parse_bot_commands(slack_client.rtm_read())
+            if command:
+                handle_command(command, channel, poster)
+            time.sleep(RTM_READ_DELAY)
+
+            # post any start messages
+            for wokr in SCHEDULE:
+                # post any start messages
+                if SCHEDULE[wokr].status == 'init':
+                    post(SCHEDULE[wokr].start_message(), channel)
+                    # run anything in the scheduler
+                    post(SCHEDULE[wokr].run(), channel)
+
+                # post any done messages
+                if SCHEDULE[wokr].status == 'done':
+                    stopmess = post(SCHEDULE[wokr].done_message(), channel)
+                    if stopmess: 
+                        post(stopmess)
+    else:
+        print("Connection failed. Exception traceback printed above.")
+
+
+
 
 ########################
 ###### Commands
@@ -115,12 +172,8 @@ def post(message):
 #     now = datetime.datetime.now()
 #     response = f"Try using one of these commands : {', '.join(list(COMMANDS.keys()))}"
 #     if options:
-#         response = "I didn't recognize that command. " + response
-#     slack_client.api_call(
-#         "chat.postMessage",
-#         channel=channel,
-#         text=response or default_response
-#     )
+        
+
 
 # def status(options):
 #     find_folder = os.path.isdir(DEFAULT_FOLDER)
@@ -142,56 +195,3 @@ def post(message):
 #         channel=channel,
 #         text=response
 #     )    
-
-########################
-###### Run
-########################
-
-# instantiate Slack client
-slack_client = SlackClient(os.environ.get('SLACK_BOT_TOKEN'))
-# autobot's user ID in Slack: value is assigned after the bot starts up
-autobot_id = None
-
-# DEFAULT_FOLDER = '/Volumes/i_bio/Crispr_F0_Screens/0-Genes_for_design/Genes_for_autobot_beta/'
-DEFAULT_FOLDER = '/mnt/i_bio/Crispr_F0_Screens/0-Genes_for_design/Genes_for_autobot_beta/'
-
-
-# constants
-RTM_READ_DELAY = 1 # 1 second delay between reading from RTM
-COMMANDS = {
-    "make_primers" : make_primers,
-    "hello": hello,
-    "help" : help,
-    "status" : status,
-}
-MENTION_REGEX = "^<@(|[WU].+?)>(.*)"
-SCHEDULE = {}
-
-if __name__ == "__main__":
-    if slack_client.rtm_connect(with_team_state=False):
-        print("AutoBot connected and running!")
-        # Read bot's user ID by calling Web API method `auth.test`
-        autobot_id = slack_client.api_call("auth.test")["user_id"]
-        while True:
-            command, channel, poster = parse_bot_commands(slack_client.rtm_read())
-            if command:
-                handle_command(command, channel)
-            time.sleep(RTM_READ_DELAY)
-
-            # post any start messages
-            for wokr in SCHEDULE:
-                # post any start messages
-                if SCHEDULE[wokr]['status'] == 'init':
-                    post(SCHEDULE[wokr]['bot'].start_message())
-                    # run anything in the scheduler
-                    post(SCHEDULE[wokr]['bot'].run(SCHEDULE)
-
-                # post any done messages
-                if SCHEDULE[wokr]['status'] == 'init':
-                    stopmess = post(SCHEDULE[wokr]['bot'].done_message())
-                    if stopmess: 
-                        post(stopmess)
-    else:
-        print("Connection failed. Exception traceback printed above.")
-
-
